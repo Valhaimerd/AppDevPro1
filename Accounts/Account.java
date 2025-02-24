9bbdbfe77ea6698a490bee2818ec95c57e31f007
KennyDaBeast <kennethcabellar@gmail.com>
2025-02-25 00:58:46 +0800

    Update Account.java

--- Accounts/Account.java
+++ Accounts/Account.java
@@ -18,6 +18,7 @@ public abstract class Account {
         this.OWNERLNAME = OWNERLNAME;
         this.OWNEREMAIL = OWNEREMAIL;
         this.pin = pin;
+        this.TRANSACTIONS = new ArrayList<>();
     }
 
     public Bank getBank() {
@@ -70,18 +71,53 @@ public abstract class Account {
 
     public String getFullName() {return OWNERLNAME + ", " + OWNERFNAME;}
 
+
+    /**
+    * Adds a new transaction to the account's transaction history.
+    *
+    * @param accountNum  The account number associated with the transaction.
+    * @param type        The type of transaction (e.g., deposit, withdrawal).
+    * @param description A brief description of the transaction.
+    */
     public void addNewTransaction(String accountNum, Transaction.Transactions type, String description) {
         // TODO Complete this method.
+        Transaction transaction = new Transaction(accountNum, type, description);
+        TRANSACTIONS.add(transaction);
     }
 
+    
+    /**
+    * Retrieves the transaction history of the account.
+    *
+    * @return A string representation of all transactions, or a message indicating no transactions are available.
+    */
     public String getTransactionInfo() {
         // TODO Complete this method.
-        return "";
+        if (TRANSACTIONS.isEmpty()) {
+            return "No transactions available.";
+        }
+        StringBuilder transactionInfo = new StringBuilder("Transaction History:\n");
+        for (Transaction t : TRANSACTIONS) { // Fixed loop variable type
+            transactionInfo.append(t.toString()).append("\n");
+        }
+        return transactionInfo.toString();
     }
 
+
+    /**
+    * Returns a string representation of the account details, including bank name, 
+    * account number, owner's full name, email, and the number of transactions.
+    *
+    * @return A formatted string with account details.
+    */
     @Override
     public String toString() {
         // TODO Complete this method.
-        return "";
+        return "Account Details:\n" +
+                "Bank: " + bank.getName() + "\n" +
+                "Account Number: " + ACCOUNTNUMBER + "\n" +
+                "Owner: " + getFullName() + "\n" +
+                "Email: " + OWNEREMAIL + "\n" +
+                "Transactions: " + TRANSACTIONS.size();
     }
 }
