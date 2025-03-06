a04c79b6c2f295924e613a3b2ebd345edf394664
bayotlang04 <bayotlanggaby@gmail.com>
2025-03-06 23:13:21 +0800

    Added a class declaration

--- Test/TransactionTest.java
+++ Test/TransactionTest.java
@@ -4,4 +4,21 @@ import java.io.*;
 import java.sql.*;
 import java.util.List;
 
+class TransactionTest {
+    private static final String TEST_CSV = "test_transactions.csv"; // CSV file for testing
+    private Connection conn; // database connection for testing
 
+@BeforeEach
+void setUp() throws SQLException {
+        // Runs before each test
+        // Creates a temporary database in memory pero wala sya g save sa file
+    conn = DriverManager.getConnection("jdbc:sqlite::memory:");
+    try (Statement stmt = conn.createStatement()) {
+        stmt.execute("CREATE TABLE Transactions (" +
+                "accountNumber TEXT, " +
+                "transactionType TEXT, " +
+                "description TEXT)");
+    }
+}
+
+}
